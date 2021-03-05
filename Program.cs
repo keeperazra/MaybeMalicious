9bb30140593fbf27df10006f3e34ea67e9271699
keeperazra <alexander.zimmerman@mymail.champlain.edu>
2021-03-04 23:19:03 -0500

    Begin implementing arguments
    This will need actual testing, as well as some figuring out how to use keys properly, and implement kwargs.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Security.Cryptography;
 
@@ -10,11 +11,13 @@ namespace MaybeMalicious
         {
             if (args.Length == 0)
             {
-                Console.WriteLine("No file selected! Exiting!");
+                PrintHelp();
                 return;
             }
 
-            string fileName = args[0];
+            Dictionary<string, string> kwargs = ProcessArgs(args);
+
+            string fileName = kwargs["target"];
             if (!File.Exists(fileName))
             {
                 Console.WriteLine(string.Format("File \"{0}\" does not exist!", fileName));
@@ -29,6 +32,80 @@ namespace MaybeMalicious
 
             EncryptFile(fileName, aes);
             DecryptFile(fileName, aes);
+        }
+
+        static Dictionary<string, string> ProcessArgs(string[] args)
+        {
+            Dictionary<string, string> kwargs = new Dictionary<string, string>
+            {
+                ["action"] = "decrypt"
+            };
+            for (int i = 0; i < args.Length; i++)
+            {
+                string arg = args[i];
+                string value = null;
+                if (arg.Contains('='))
+                {
+                    value = arg.Split('=')[1];
+                    arg = arg.Split('=')[0];
+                }
+                else if (i + 1 < args.Length && args[i + 1][0] != '-')
+                {
+                    // The next index of args exists and is not a parameter, therefore it should be treated as a value
+                    i++;
+                    value = args[i];
+                }
+                switch (arg)
+                {
+                    case "-f":
+                        kwargs["target"] = value ?? throw new ArgumentException("Detected -f, but no file name was supplied!");
+                        kwargs["type"] = "file";
+                        break;
+                    case "-d":
+                        kwargs["target"] = value ?? throw new ArgumentException("Detected -d, but no directory path was supplied!");
+                        kwargs["type"] = "directory";
+                        break;
+                    case "--key":
+                        kwargs["key"] = value ?? throw new ArgumentException("Detected --key, but no key was provided!");
+                        break;
+                    case "--keyfile":
+                        kwargs["key"] = FetchKeyFromFile(value);
+                        break;
+                    case "-e":
+                        kwargs["action"] = "encrypt";
+                        break;
+                    default:
+                        Console.WriteLine($"Ignoring unknown parameter \"{arg}\"");
+                        break;
+                }
+            }
+            return kwargs;
+        }
+
+        static void PrintHelp()
+        {
+            Console.WriteLine("Expected syntax: MaybeMalicious [-f | -d] <path> [--key=<key> | --keyfile=<key_file>]");
+            Console.WriteLine();
+            Console.WriteLine("--key       to supply the private key to decrypt your files.");
+            Console.WriteLine("--keyfile   to supply the path to a file that contains the private key.");
+            Console.WriteLine("-f          to decrypt the specified file using the provided key.");
+            Console.WriteLine("-d          to decrypt all the files in the specified directory.");
+            Console.WriteLine();
+            Console.WriteLine("Secret options: -e");
+            Console.WriteLine("These will encrypt your files!");
+        }
+
+        static string FetchKeyFromFile(string filePath)
+        {
+            if (!File.Exists(filePath))
+            {
+                throw new ArgumentException("Cannot find key file at " + filePath);
+            }
+
+            using FileStream file = new FileStream(filePath, FileMode.Open);
+            using StreamReader read = new StreamReader(file);
+
+            return read.ReadToEnd();
         }
 
         static void EncryptFile(string filePath, Aes aes)

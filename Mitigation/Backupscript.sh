63dc5d131eda1154a5063c6bb39b77d5af24503a
ChampATW <47860671+ChampATW@users.noreply.github.com>
2021-03-06 17:11:27 -0500

    Delete Backupscript.sh

--- Mitigation/Backupscript.sh
+++
@@ -1,9 +0,0 @@
-echo mounting drive
-sudo mount -a
-echo copying data from Ipotat to ipotat2
-cp /home/victim/Desktop/Ipotat/*.* /home/victim/Desktop/ipotat2
-echo setting ownership
-chown root:root /home/victim/Desktop/ipotat2 -R
-chmod 740 /home/victim/Desktop/ipotat2 -R
-echo unmounting backup
-umount /dev/sdb
